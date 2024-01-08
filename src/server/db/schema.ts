d6b40f0a2f005971072650e72357d7b63d11cfb5
Vikrant Bhat <vik.bhat223@gmail.com>
2024-01-08 14:09:45 +0530

    Update database driver and dependencies, create database schema and tables

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,16 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
 import {
-  bigint,
-  index,
-  mysqlTableCreator,
+  integer,
+  pgEnum,
+  pgTable,
+  pgTableCreator,
+  serial,
+  text,
   timestamp,
   varchar,
-} from "drizzle-orm/mysql-core";
+} from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -16,19 +18,26 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const mysqlTable = mysqlTableCreator((name) => `chat-pdf_${name}`);
 
-export const posts = mysqlTable(
-  "post",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt").onUpdateNow(),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const mypgTable = pgTableCreator((name) => `chat-pdf_${name}`);
+
+export const userSystemEnum = pgEnum("user_system_enum", ["user", "system"]);
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf_name").notNull(),
+  pdfUrl: text("pdf_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  fileKey: text("file_key").notNull(), // for retrieveing the file from s3
+});
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: userSystemEnum("role").notNull(),
+});
